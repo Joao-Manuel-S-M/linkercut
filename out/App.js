993e2a14c534059fb70486bba1919b63208aae30
João Manuel <jmsmarques8213@gmail.com>
2024-04-03 18:23:50 -0300

    update

---
+++ out/App.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+//pages
+const BuildingPage_1 = require("./pages/BuildingPage");
+function App() {
+    return ((0, jsx_runtime_1.jsx)(jsx_runtime_1.Fragment, { children: (0, jsx_runtime_1.jsx)(BuildingPage_1.default, {}) }));
+}
+exports.default = App;
